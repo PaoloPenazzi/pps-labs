dc2c1dd9515d60766fe4e9c3bb8fff78869855d9
paolo <paolo.penazzi@studio.unibo.it>
2022-03-29 09:59:22 +0200

    Create ConferenceReviewing trait

---
+++ lab5/ConferenceReviewing.scala
@@ -0,0 +1,22 @@
+package u05lab.ex2
+
+enum Question:
+  case RELEVANCE
+  case SIGNIFICANCE
+  case CONFIDENCE
+  case FINAL
+
+trait ConferenceReviewing:
+  def loadReview(article: Int, map: Map[Question ,Int]): Unit
+
+  def loadReview(article: Int, rel: Int, sig: Int, conf: Int, fin: Int): Unit
+
+  def orderedScores(article: Int, question: Question): List[Int]
+
+  def averageFinalScore(article: Int): Double
+
+  def acceptedArticle(): Set[Int]
+
+  def sortedAcceptedArticles(): List[(Int, Double)]
+
+  def averageWeightedFinalScoreMap(): Map[Int, Double]
