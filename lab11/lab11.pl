e84feb7013a2d9b0e7264141e8a10b40a177c5f4
paolo <paolo.penazzi@studio.unibo.it>
2022-05-17 18:40:07 +0200

    Upload exercises done

--- lab11/lab11.pl
+++ lab11/lab11.pl
@@ -28,16 +28,19 @@ in_degree([e(_,E)|T], E, N) :- in_degree(T, E, M), N is M + 1, !.
 in_degree([H|T], E, M) :- in_degree(T, E, M).
 ?- in_degree([e(1,2), e(1,3), e(3,2)], 3, X).
 
+% Solution without using findall and member
 reaching([e(X,T)], X, T).
 reaching([e(X, N)|T], X, [N|L]) :- reaching(T, X, L), !.
 reaching([_|T], X, L) :- reaching(T, X, L).
-% why the output is [1,2,3|4] ???
 
+% Using findall and member
+reaching(L, N, O) :- findall(X, member(e(N,X), L), O).
 
 anypath(L, N1, N2, O) :- anypath(L, N1, N2, O, L).
 anypath([e(N1,N2)|_], N1, N2, e(N1,N2), IL).
 anypath([e(N,N2)|_], N1, N2, [Z|e(N,N2)], IL) :- anypath(IL, N1, N, Z).
 anypath([_|T], N1, N2, Z, IL) :- anypath(T, N1, N2, Z, IL).
+?- anypath([e(1,3),e(2,5),e(3,8),e(1,8),e(8,7),e(1,7)], 1, 7, O)
 
 allreaching([], N, []).
 allreaching(L, N, Bag) :- findall(e(N,X), member(e(N,X),L), Bag).
@@ -47,5 +50,7 @@ allreaching(L, N, Bag) :-
     findall(X, member(e(N,X),L), [B1|B2]),
     allreaching(e(X,Y), member(e(X,Y), L), B2).
 
+
+?- findall(e(1,X), member(X,[1,2,3,4]), Bag)
 
 
